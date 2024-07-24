8a39983956f45980cddf24ff8875772c2f785196
ronhoover97 <159500719+ronhoover97@users.noreply.github.com>
2024-07-23 21:54:31 -0400

    Signup
    Signup Front end

---
+++ Signup.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { Button, Form, Input } from "antd";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+const Signup = () => (
+  <Form
+    name="signup"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: "Please input your username!",
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Email"
+      name="email"
+      rules={[
+        {
+          required: true,
+          message: "Please input your email!",
+        },
+        {
+          type: "email",
+          message: "The input is not valid E-mail!",
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: "Please input your password!",
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      label="Confirm Password"
+      name="confirm"
+      dependencies={["password"]}
+      hasFeedback
+      rules={[
+        {
+          required: true,
+          message: "Please confirm your password!",
+        },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue("password") === value) {
+              return Promise.resolve();
+            }
+            return Promise.reject(
+              new Error("The two passwords that you entered do not match!")
+            );
+          },
+        }),
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Signup
+      </Button>
+    </Form.Item>
+  </Form>
+);
+
+export default Signup;
