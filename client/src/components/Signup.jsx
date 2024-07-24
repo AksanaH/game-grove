2e6653cdaf082d8d904574f0b5209204ada2edd3
ronhoover97 <159500719+ronhoover97@users.noreply.github.com>
2024-07-23 21:21:42 -0400

    Created Signup Page

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100 vw-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Register</h2>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              autoComplete="off"
+              id="name"
+              name="name"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="off"
+              id="email"
+              name="email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              autoComplete="off"
+              id="password"
+              name="password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Register
+          </button>
+          <p>Already Have an Account?</p>
+          <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
