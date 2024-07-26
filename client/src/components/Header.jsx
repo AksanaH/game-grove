469a546ec42456a325956271eb4a8e9c04560b85
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-25 20:06:48 -0500

    created navbar linking to login and home

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Col, Divider, Row } from "antd";
+import { Link } from "react-router-dom";
+import "./Header.css";
+const Navbar = () => {
+  return (
+    <Row justify="end" className="header" align="middle">
+      <Col span={2}>
+        <Link className="navlink" to="/">
+          Games
+        </Link>
+      </Col>
+      <Col span={2}>
+        <Link className="navlink" to="/search">
+          Search
+        </Link>
+      </Col>
+      <Col span={2}>
+        <Link className="navlink" to="/mygames">
+          My Games
+        </Link>
+      </Col>
+      <Col span={2}>
+        <Link className="navlink" to="/profile">
+          Profile
+        </Link>
+      </Col>
+      <Col span={2}>
+        <Link className="navlink" to="/login">
+          Login
+        </Link>
+      </Col>
+    </Row>
+  );
+};
+export default Navbar;
