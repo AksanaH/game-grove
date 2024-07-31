6d10be78337ec86bd2b9d5b81236398658be2062
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-31 14:30:14 -0500

    removed dummy data and created mapping from api data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row } from "antd";
+import { Col, Row, Grid } from "antd";
 import Gamecard from "../components/Gamecard";
 import { getAllGames } from "../utils/API";
 
@@ -32,63 +32,7 @@ export default function Home() {
     },
   ]);
 
-  const [items2, setItems2] = useState([
-    {
-      title: "Item 6",
-      description: "Description for item 6",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 7",
-      description: "Description for item 7",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 8",
-      description: "Description for item 8",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 9",
-      description: "Description for item 9",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 10",
-      description: "Description for item 10",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-  ]);
-
-  const [items3, setItems3] = useState([
-    {
-      title: "Item 11",
-      description: "Description for item 11",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 12",
-      description: "Description for item 12",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 13",
-      description: "Description for item 13",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 14",
-      description: "Description for item 14",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-    {
-      title: "Item 15",
-      description: "Description for item 15",
-      image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-    },
-  ]);
-
-const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -97,68 +41,41 @@ const [games, setGames] = useState([]);
         if (!response.ok) {
           throw new Error("Network response was not ok.");
         }
-        const {results} = await response.json();
+        const { results } = await response.json();
         console.log(results);
         setGames(results);
       } catch (error) {
         console.error("Error fetching games:", error);
       }
-    }
+    };
     fetchGames();
   }, []);
-      
 
   return (
-    <>
+    <div className="content">
+      <div className="text-center">
+        <Row gutter={[20, 10]}>
+          <Col span={24}>
+            <h1>Welcome Gamers! </h1>
+          </Col>
+        </Row>
+      </div>
       <div className="popular-games">
-        <h1>Popular Games</h1>
       </div>
       <div>
-        <Row gutter={[20, 10]}justify = "center">
+        <Row gutter={[20, 10]} justify="center">
           {games.map((item, index) => (
             <Col key={index} xs={24} sm={24} md={8} lg={6} xl={4}>
               <Gamecard
                 title={item.name}
                 description={`Genre: ${item.genres[0].name} |
-                ★ ${item.rating}` }
+                ★ ${item.rating}`}
                 image={item.background_image}
               />
             </Col>
           ))}
         </Row>
       </div>
-      <div className="popular-games">
-        <h1>Popular Games</h1>
-      </div>
-      <div>
-        <Row gutter={[20, 10]}justify="center">
-          {items2.map((item, index) => (
-            <Col key={index} xs={24} sm={24} md={8} lg={6} xl={4}>
-              <Gamecard
-                title={item.title}
-                description={item.description}
-                image={item.image}
-              />
-            </Col>
-          ))}
-        </Row>
-      </div>
-      <div className="popular-games">
-        <h1>Popular Games</h1>
-      </div>
-      <div >
-        <Row gutter={[20, 10]}justify="center">
-          {items3.map((item, index) => (
-            <Col key={index} xs={24} sm={24} md={8} lg={6} xl={4}>
-              <Gamecard
-                title={item.title}
-                description={item.description}
-                image={item.image}
-              />
-            </Col>
-          ))}
-        </Row>
-      </div>
-    </>
+    </div>
   );
 }
